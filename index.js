dbd4cc3b5527bc5c3785ec01016fbc69a36f6b76
DecentM <DecentM@users.noreply.github.com>
2017-06-20 13:55:52 +0200

    Use local database and caching initial

--- index.js
+++ index.js
@@ -1,153 +1,104 @@
 /* jshint esversion:6, undef: false */
 
-var dnsServer = require('dns-express')();
-var isReachable = require('is-reachable');
-var dns = require('dns');
-var address = require('address');
-var sleep = require('sleep');
-var eventemitter = require('events');
-var config = require('./config');
-var db = require('./database');
+const dns = require('dns')
+const dnsExpress = require('dns-express')
+const Loki = require('lokijs')
+const chalk = require('chalk')
+const Q = require('q')
 
-class myEmitter extends eventemitter {}
-var event = new myEmitter();
+const server = dnsExpress()
+const log = (content) => {
+  // eslint-disable-next-line no-console
+  console.log(chalk.cyan(JSON.stringify(content)))
+}
+const config = require('./config.json')
 
-function netstatus(onConnect, onDisconnect) {
-    var s = {};
+let records = null
+const databaseInitialize = () => {
+  records = db.getCollection('records')
+  if (records === null) {
+    records = db.addCollection('records')
+  }
 
-    function expand(toExpand, func) {
-        s[toExpand] = func;
-    }
-    var statusCategories = [
-        'bool',
-        'string'
-    ];
-    for (var i in statusCategories) {
-        expand(statusCategories[i], {});
-    }
-
-    if (typeof address.ip() === "undefined") { // NO NETWORK CONNECTION
-        s.bool.network = false;
-        s.string.ip4 = "127.0.0.1";
-        s.string.ip6 = "::1";
-        s.bool.dns = false;
-        if (typeof onDisconnect === "function") {
-            onDisconnect();
-        }
-    } else { // NETWORK CONNECTION AVAILABLE
-        s.bool.network = true;
-        s.string.ip4 = address.ip();
-        s.string.ip6 = address.ipv6();
-        isReachable('https://google.com:443').then(online => { // INTERNET CONNECTION AVAILABLE
-            if (online) {
-                s.bool.dns = true;
-            } else {
-                s.bool.dns = false;
-            }
-        });
-        if (typeof onConnect === "function") {
-            onConnect();
-        }
-    }
-    return s;
+  runServer()
 }
 
-event.on('networkDown', function () {
-    console.log('Waiting for network...');
-});
+const db = new Loki('nodeblock.db', {
+  'autoload':         true,
+  'autoloadCallback': databaseInitialize,
+  'autosave':         true,
+  'autosaveInterval': 2000
+})
 
-event.on('networkUp', function () {
-    console.log('Network connection established');
-    console.log('IPV4 address: ' + netstatus().string.ip4);
-    console.log('IPV6 address: ' + netstatus().string.ip6);
-});
+const requestRecord = (domain) => {
+  const deferred = Q.defer()
 
-function networkCheck(initial) {
-    if (netstatus().bool.network === false) {
-        event.emit('networkDown');
-        while (netstatus().bool.network === false) {
-            sleep.msleep(2000);
-        }
-        if (!initial) {
-            event.emit('networkUp');
-        }
+  dns.resolve(domain, (err, content) => {
+    if (err) {
+      deferred.reject(err)
+    } else {
+      deferred.resolve(content)
     }
-
-    if (initial) {
-        if (netstatus().bool.network === true) {
-            event.emit('networkUp');
-        }
-    }
+  })
+  return deferred.promise
 }
 
-networkCheck(true);
-setInterval(function () {
-    networkCheck();
-}, 5000);
-
-var blocklist = [
-    'domain.com',
-    'test.com',
-    'example.com'
-];
-
-function testarray(array, query) {
-    if (array.includes(query)) {
-        return true;
-    }
+const storeRecord = (record) => {
+  if (typeof record === 'object') {
+    log(`Storing record of ${record.name}, ${record.value} in database`)
+    records.insert(record)
+  } else {
+    throw new TypeError(`Not storing invalid record ${JSON.stringify(record)}`)
+  }
 }
 
-function reply(res, name, ip, ip6) {
-    res.a({
-        name: name,
-        address: ip,
-        ttl: 600
-    });
-    if (typeof ip6 === "string") {
-        res.aaaa({
-            name: name,
-            address: ip6,
-            ttl: 600
-        });
-    }
+const runServer = () => {
+  server.listen(config.port)
 }
 
-dnsServer.a(function (req, res, next) {
-    isReachable('google.com').then(online => {
-        console.log('Online status: ' + online);
-        if (online) {
-            if (testarray(blocklist, req.name)) {
-                console.log('Domain ' + req.name + ' is blocked');
-                console.log('Changing A record for ' + req.name);
-                //Add an A record to the response's answer.
+server.use((req, res, next) => {
+  if (req.questions.length > 1) {
+    throw new Error('More than one question in one query is not supported')
+  } else {
+    const question = req.questions[0]
+    const dbResult = records.find({'name': question.name})
 
-                reply(res, req.name, address.ip(), address.ipv6());
-                return res.end();
-            } else { // Domain not blocked
-                console.log('Domain ' + req.name + ' is NOT blocked');
-                dns.lookup(req.name, {
-                    all: false
-                }, (err, addresses, family) => {
-                    console.log('Sending real data for ' + req.name + ': ' + addresses);
-                    reply(res, req.name, addresses);
-                    return res.end();
-                });
-            }
-        } else { // No internet
-            console.log('Online status: ' + online);
-            console.log('Responding to ' + req.remote.address + ', who is asking for ' + req.name);
-
-            reply(res, req.name, address.ip(), address.ipv6());
-            return res.end();
+    if (dbResult.length > 0) {
+      log(`Response for ${question.name} found in database, responding with ${dbResult[0].value}, then querying`)
+      res.a({
+        'name':    question.name,
+        'address': dbResult[0].value,
+        'ttl':     600
+      })
+      res.end()
+      requestRecord(question.name)
+        .then((value) => {
+          log(`Async remote query complete, got ${value[0]}`)
+          storeRecord({
+            'name':  question.name,
+            'value': value[0]
+          })
+        })
+    } else {
+      log(`Response for ${question.name} not found in database, querying`)
+      dns.resolve(question.name, (err, content) => {
+        if (err) {
+          log('An error occurred')
+          throw err
+        } else {
+          log(`Sync remote query complete, got ${content[0]}`)
+          res.a({
+            'name':    question.name,
+            'address': content[0],
+            'ttl':     600
+          })
+          res.end()
+          storeRecord({
+            'name':  question.name,
+            'value': content[0]
+          })
         }
-    });
-});
-
-dnsServer.use(function (req, res) {
-    //End the response if no "routes" are matched
-    res.end();
-});
-
-dnsServer.listen(53535, function () {
-    console.log('DNS server is listening');
-});
+      })
+    }
+  }
+})
