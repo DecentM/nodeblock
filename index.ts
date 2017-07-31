a50b328bd2fcd5d406595c0ad53779689530ac62
DecentM <decentm@decentm.com>
2017-07-31 21:42:46 +0200

    Only reply to local addresses (configurable)

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { config } from './config'
 
 import {
-  dbPromise,
   records,
   getLocalRecord,
   storeRecord
@@ -18,6 +17,7 @@ import * as Ifaces from './interfaces'
 
 import * as log from 'chalk-console'
 import * as dns from 'dns'
+import * as ipRangeCheck from 'ip-range-check'
 
 log.info('Nodeblock is starting')
 
@@ -54,72 +54,80 @@ server.use((packet, respond, next) => {
   // We only support one question, so we make sure we only have one
   const question = packet.questions[0]
 
-  // Request a record from wherever
-  requestRecord(question)
-  .then((replies: Ifaces.IfinalReply) => {
-    getReverseHost(question.remote.address)
-    .then((hostname: Array<any>) => {
-      if (hostname.length === 0) {
-        hostname[0] = question.remote.address
-      }
-      log.info(`Resolved ${replies.length} record(s) for ${hostname} from ${replies.source}
-    Domain: ${question.name}
-      `)
-    })
-    .catch((error) => {
-      handle(error)
-    })
-
-    switch (replies.source) {
-    case 'online':
-      replies.forEach((reply) => {
-        respond[reply.type.toLowerCase()](reply)
-        storeRecord(reply)
-        .catch((error) => {
-          handle(error)
-        })
+  if (!(ipRangeCheck(question.remote.address, config.get('answerRange')))) {
+    handle(new Error(`Client address is not in the permitted answer range(s):
+    Address: ${question.remote.address},
+    Range(s): ${config.get('answerRange')}
+    `))
+  } else {
+    // Request a record from wherever
+    requestRecord(question)
+    .then((replies: Ifaces.IfinalReply) => {
+      getReverseHost(question.remote.address)
+      .then((hostname: Array<any>) => {
+        if (hostname.length === 0) {
+          hostname[0] = question.remote.address
+        }
+        log.info(`Resolved ${replies.length} record(s) for ${hostname} from ${replies.source}
+        Domain: ${question.name}
+        `)
       })
-      break
-    case 'database':
-      replies.forEach((reply) => {
-        respond[reply.type.toLowerCase()](reply)
-        getRemoteRecord(question)
-        .then((remoteReplies: Array<Ifaces.InternalAnswer>) => {
-          records.findAndRemove({
-            'type': replies[0].type,
-            'name': replies[0].name
+      .catch((error) => {
+        handle(error)
+      })
+
+      switch (replies.source) {
+        case 'online':
+        replies.forEach((reply) => {
+          respond[reply.type.toLowerCase()](reply)
+          storeRecord(reply)
+          .catch((error) => {
+            handle(error)
           })
-          remoteReplies.forEach((remoteReply) => {
-            storeRecord(remoteReply)
-            .catch((error) => {
-              handle(error)
+        })
+        break
+        case 'database':
+        replies.forEach((reply) => {
+          respond[reply.type.toLowerCase()](reply)
+          getRemoteRecord(question)
+          .then((remoteReplies: Array<Ifaces.InternalAnswer>) => {
+            records.findAndRemove({
+              'type': replies[0].type,
+              'name': replies[0].name
+            })
+            remoteReplies.forEach((remoteReply) => {
+              storeRecord(remoteReply)
+              .catch((error) => {
+                handle(error)
+              })
             })
           })
+          .catch((error) => {
+            handle(error)
+          })
         })
-        .catch((error) => {
-          handle(error)
-        })
-      })
-      break
-    default:
-      log.error(`DNS reply source is neither 'online', or 'database'
+        break
+        default:
+        log.error(`DNS reply source is neither 'online', or 'database'
         ${new Error().stack}
-      `)
-      break
-    }
-    respond.end()
-  })
-  .catch((error) => {
-    switch (error.code) {
-    // If we get an answer saying that the record doesn't exist, we suppress
-    // the error message and reply with an empty record
-    case 'ENODATA':
+        `)
+        break
+      }
       respond.end()
-      break
-    default:
-      handle(error)
-      respond.end()
-      break
-    }
-  })
+    })
+    .catch((error) => {
+      switch (error.code) {
+        // If we get an answer saying that the record doesn't exist, we suppress
+        // the error message and reply with an empty record
+        case 'ENODATA':
+        respond.end()
+        break
+        default:
+        handle(error)
+        respond.end()
+        break
+      }
+    })
+  }
+
 })
